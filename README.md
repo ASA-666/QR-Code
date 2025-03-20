cf5a0c66406a0a432e7924ad037dca86f0b05d4f
ASA666 <srisab.work@gmail.com>
2025-03-20 14:20:41 +0545

    Edits to the directory; cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Test
+# QR Code Generator
+
+This is a simple Python application to generate QR codes.
+
+## Requirements
+
+- Python 3.x
+- `qrcode` library
+- `Pillow` library
+
+## Installation
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/ASA-666/QR-Code.git
+    cd QR-Code
+    ```
+
+2. Install the required libraries:
+    ```sh
+    pip install qrcode[pil]
+    ```
+
+## Usage
+
+1. Run the script:
+    ```sh
+    python gen_qr_code.py
+    ```
+
+2. Enter the data you want to encode in the QR code when prompted.
+
+3. The generated QR code will be saved as `qrcode.png` in the current directory.
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- gen_qr_code.py
+++ gen_qr_code.py
@@ -1,25 +1,25 @@
 import qrcode
 
-#data to be encoded in the qrcode
+# Data to be encoded in the qrcode
 
-data = "https://www.youtube.com/watch?v=xvFZjo5PgG0"
+data = input(str("Enter the data to be encoded in the QR code: "))
 
-#ceeate a qrcode instance
+# Create a QR Code instance
 qr= qrcode.QRCode(
     version=1, #controls the size of qr code
     error_correction=qrcode.constants.ERROR_CORRECT_L, #corrects error correction level
     box_size=10,
     border=4,
 )
-#add data to the qr code
+# Add data to the qr code
 qr.add_data(data)
 qr.make(fit=True)
 
-#create an image from the qr code
+# Create an image from the qr code
 img= qr.make_image(fill='black', black_color='white')
 
-#save the image
+# Save the image
 img.save("qrcode_example.png")
 
-#display the image
+# Display the image
 img.show()
